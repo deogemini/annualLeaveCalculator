cdfaeb8161c7a15943816552e10fbece4958b08d
Deo Gemini <geminideogratias8@gmail.com>
2024-05-16 22:20:08 +0300

    workdays automatical
    hatuingizi manualy is direct automated

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -8,8 +8,6 @@
     <input type="date" id="end_date" name="end_date" required><br><br>
     <label for="accrual_rate">Accrual Rate:</label>
     <input type="number" step="0.001" id="accrual_rate" name="accrual_rate" required><br><br>
-    <label for="days_in_month">Working Days in a Month:</label>
-    <input type="number" step="0.001" id="days_in_month" name="days_in_month" required><br><br>
     <button type="submit">Calculate</button>
 </form>
 
